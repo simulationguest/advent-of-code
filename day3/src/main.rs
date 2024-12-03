c877e2699558244d94c5bd364275e434de6858cc
Leopold Jofer <projekte@leoj.de>
2024-12-03 14:19:57 -0800

    add second part of exercise

--- day3/src/main.rs
+++ day3/src/main.rs
@@ -3,15 +3,31 @@ use regex::Regex;
 fn main() {
     let input = include_str!("./input.txt");
 
-    let regex = Regex::new(r"(?m)mul\((\d+),(\d+)\)").unwrap();
-    let sum: i32 = regex
-        .captures_iter(input)
-        .map(|cap| {
-            let a: i32 = cap[1].parse().unwrap();
-            let b: i32 = cap[2].parse().unwrap();
-            a * b
+    let do_regex = Regex::new(r"(?s)(?:(?:do\(\))|^).*?(?:(?:don't\(\))|$)").unwrap();
+    let mul_regex = Regex::new(r"(?m)mul\((\d+),(\d+)\)").unwrap();
+
+    let total_sum = sum_muls(&mul_regex, input);
+    println!("Total sum: {total_sum}");
+
+    let partial_sum: i32 = do_regex
+        .find_iter(input)
+        .map(|m| {
+            let m = m.as_str();
+            sum_muls(&mul_regex, m)
         })
         .sum();
+    println!("Partial sum: {partial_sum}");
+}
 
-    println!("Sum: {sum}");
+
+// pass the regex to the function to avoid reinitializing it on every call
+fn sum_muls(re: &Regex, m: &str) -> i32 {
+    re 
+                .captures_iter(m)
+                .map(|cap| {
+                    let a: i32 = cap[1].parse().unwrap();
+                    let b: i32 = cap[2].parse().unwrap();
+                    a * b
+                })
+                .sum()
 }
